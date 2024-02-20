aad5c8edb37f280b278af7d636f3b7d4d0f06e56
Michal Paleczny <paleczny.michal@gmail.com>
2024-02-20 17:21:54 +0000

    Added project pictures and done implementation of it

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,42 +10,42 @@ const projects = [
     {
         title: "Team Profile Generator",
         description: "Generate team profiles using Node.js",
-        image: "https://via.placeholder.com/150",
+        image: "src/assets/cards/Team Profile Generator.png",
         deployedLink: "https://mrkatrish.github.io/team_profile_generator/",
         githubLink: "https://github.com/MrKatrish/team_profile_generator"
     },
     {
         title: "README Generator",
         description: "Generate README files using Node.js",
-        image: "https://via.placeholder.com/150", 
+        image: "src/assets/cards/README Generator.png", 
         deployedLink: "https://github.com/MrKatrish/README_Generator",
         githubLink: "https://github.com/MrKatrish/README_Generator"
     },
     {
         title: "CurioCity",
         description: "Explore various cities around the world",
-        image: "https://via.placeholder.com/150",
+        image: "src/assets/cards/CurioCity.png",
         deployedLink: "https://mrkatrish.github.io/CurioCity/",
         githubLink: "https://github.com/MrKatrish/CurioCity"
     },
     {
         title: "Weather Dashboard",
         description: "Check the weather forecast for any city",
-        image: "https://via.placeholder.com/150",
+        image: "src/assets/cards/Weather Dashboard.png",
         deployedLink: "https://mrkatrish.github.io/weather_dashboard/",
         githubLink: "https://github.com/MrKatrish/weather_dashboard"
     },
     {
         title: "Work Day Scheduler",
         description: "Plan your workday efficiently",
-        image: "https://via.placeholder.com/150", 
+        image: "src/assets/cards/Work Day Schedule.png", 
         deployedLink: "https://mrkatrish.github.io/work_day_scheduler/",
         githubLink: "https://github.com/MrKatrish/work_day_scheduler"
     },
     {
         title: "Code Quiz",
         description: "Test your coding knowledge with a quiz",
-        image: "https://via.placeholder.com/150", 
+        image: "src/assets/cards/Code Quiz.png", 
         deployedLink: "https://mrkatrish.github.io/code_quiz/",
         githubLink: "https://github.com/MrKatrish/code_quiz"
     }
